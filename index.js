8c4bd34efa1b91219bec68a396b3890eb14cef34
GuruhTech <guruhtechlab@gmail.com>
2026-03-28 21:57:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,50 @@
-const express = require('express');
+import express from 'express';
+import bodyParser from 'body-parser';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import fs from 'fs';
+
 const app = express();
-__path = process.cwd()
-const bodyParser = require("body-parser");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+global.__path = __dirname;
+
 const PORT = process.env.PORT || 8000;
-let server = require('./qr'),
-    code = require('./pair');
-require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/qr', server);
-app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/main.html')
-})
+
+// Dynamic imports for the other files
+let server, code;
+
+// Import the other files dynamically
+const loadModules = async () => {
+    server = (await import('./qr.js')).default;
+    code = (await import('./pair.js')).default;
+    
+    app.use('/qr', server);
+    app.use('/code', code);
+};
+
+loadModules();
+
+import('events').then(events => {
+    events.EventEmitter.defaultMaxListeners = 500;
+});
+
+app.use('/pair', async (req, res, next) => {
+    res.sendFile(join(__path, 'pair.html'));
+});
+
+app.use('/', async (req, res, next) => {
+    res.sendFile(join(__path, 'main.html'));
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.listen(PORT, () => {
     console.log(`
 Don't Forgot To Give Star
 
- Server running on http://localhost:` + PORT)
-})
+ Server running on http://localhost:` + PORT);
+});
 
-module.exports = app
+export default app;
